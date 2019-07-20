47d4ae0615a065b723fd44ac4720cac6e1f85fa3
Alex Ginns <alexginns@gmail.com>
2019-07-20 22:14:47 +1000

    feat: title, subtitle, github link

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Merriweather|Montserrat:900|Ubuntu+Mono&display=swap' }
     ]
   },
   /*
@@ -23,6 +24,7 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/styles/global.scss'
   ],
   /*
   ** Plugins to load before mounting the App
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,41 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        easy-share
-      </h1>
-      <h2 class="subtitle">
-        Demo for easy-social-share-links
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
+  <div>
+    <GithubCorner />
+    <div class="container">
+      <h1 class="title">Easy Share 🚀</h1>
+      <p class="intro-text">
+        Made by 
+        <a target="_blank" class="twitter-link" href="https://twitter.com/alexginns">
+          Alex Ginns
+        </a> 
+        ~ with 
+        <a target="_blank" class="npm-link" href="https://www.npmjs.com/package/easy-social-share-links">
+          easy-social-sharelinks
         </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import GithubCorner from '~/components/GithubCorner.vue';
 
 export default {
   components: {
-    Logo
+    GithubCorner
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<style lang="scss" scoped>
+.intro-text{
   text-align: center;
+  margin-top: 10px;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.twitter-link {
+  color: #1DA1F2;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.npm-link {
+  color: #fb3e44;
 }
 </style>
