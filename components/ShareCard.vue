02dd9ae33cc0f3e053f184926cff8e6304e91434
Alex Ginns <alexginns@gmail.com>
2019-07-21 16:45:22 +1000

    refactor: styles and share card layout

--- components/ShareCard.vue
+++ components/ShareCard.vue
@@ -1,161 +1,146 @@
 <template>
-    <div class="share-card">
-        <div class="share-card__link-container">
-            <img class="share-card__logo" :src="logo" :alt="alt">
-            <input :class="type" class="share-card__input" type="text" placeholder="// link" />
-        </div>
-        <div v-if="type === 'tw' || type === 'em'" class="share-card__extra-text">
-            <textarea :class="type" placeholder="// message"></textarea>
-        </div>
-        <div class="share-card__link-container">
-            <input disabled :class="type" class="code-block" placeholder="// result" />
-            <button :class="type" class="share-card__test-btn">TEST</button>
-        </div>
+  <div class="share-card" :class="type">
+    <div class="share-card__link-container">
+      <img class="share-card__logo" :src="logo" :alt="alt" />
+      <input class="share-card__input" type="text" placeholder="// link" />
     </div>
+    <div v-if="type === 'tw' || type === 'em'" class="share-card__extra-text">
+      <input
+        v-if="type === 'em'"
+        class="share-card__input subject"
+        type="text"
+        placeholder="// subject"
+      />
+      <textarea :placeholder="type === 'tw' ? '// message' : '// body'"></textarea>
+    </div>
+    <div class="share-card__link-container">
+      <input disabled class="code-block" placeholder="// result" />
+      <button class="share-card__test-btn">TEST</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        logo: {
-            type: String,
-            required: true
-        },
-        alt: {
-            type: String,
-            required: true
-        },
-        type: {
-            type: String,
-            required: true
-        }
+  props: {
+    logo: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.share-card{
-    margin-top: 60px;
-    &__link-container{
-        display: flex;
-        align-items: center;
-        &:nth-child(2) {
-            margin-top: 20px;
-        }
-    }
-    &__logo {
-        width: 50px;
-        height: 50px;
-        margin-right: 20px;
-    }
+@mixin cardType($color, $colorO) {
+  .share-card {
     &__input {
-        height: 50px;
-        border-radius: 4px;
-        font-size: 1.3em;
-        padding: 0 15px;
-        width: 100%;
-        &.fb{
-            border: 4px solid var(--fb-o);
-            &:focus{
-                border: 4px solid var(--fb);
-                outline: none;
-            }
-        }
-        &.tw{
-            border: 4px solid var(--tw-o);
-            &:focus{
-                border: 4px solid var(--tw);
-                outline: none;
-            } 
-        }
-        &.li{
-            border: 4px solid var(--li-o);
-            &:focus{
-                border: 4px solid var(--li);
-                outline: none;
-            } 
-        }
-        &.em{
-            border: 4px solid var(--em-o);
-            &:focus{
-                border: 4px solid var(--em);
-                outline: none;
-            } 
-        }
+      border: 4px solid $colorO;
+      &:focus {
+        border: 4px solid $color;
+        outline: none;
+      }
     }
-    &__extra-text{
-        & textarea{
-            height: 100px;
-            border-radius: 4px;
-            resize: none;
-            width: 100%;
-            margin: 20px 0;
-            font-size: 1.3em;
-            padding: 10px 15px;
-            font-family: 'Ubuntu Mono', monospace;
-            &.tw{
-                border: 4px solid var(--tw-o);
-                &:focus{
-                    border: 4px solid var(--tw);
-                    outline: none;
-                } 
-            }
-            &.em{
-                border: 4px solid var(--em-o);
-                &:focus{
-                    border: 4px solid var(--em);
-                    outline: none;
-                } 
-            }
+    &__extra-text {
+      & textarea {
+        border: 4px solid $colorO;
+        &:focus {
+          border: 4px solid $color;
+          outline: none;
         }
+      }
     }
-    &__test-btn{
-        height: 50px;
-        border-radius: 4px;
-        color: #fff;
-        margin-left: 20px;
-        font-size: 1.3em;
-        font-family: 'Ubuntu Mono', monospace;
-        padding: 0 20px;
-        border: none;
-        &.fb{
-            background: var(--fb);
-        }
-        &.tw{
-            background: var(--tw);
-        }
-        &.li{
-            background: var(--li);
-        }
-        &.em{
-            background: var(--em);
-        }
+    &__test-btn {
+      background: $color;
     }
+  }
+  .code-block {
+    border: 4px solid $color;
+  }
 }
-
-.code-block {
+.share-card {
+  margin-top: 60px;
+  &.fb {
+    @include cardType(var(--fb), var(--fb-o));
+  }
+  &.tw {
+    @include cardType(var(--tw), var(--tw-o));
+  }
+  &.li {
+    @include cardType(var(--li), var(--li-o));
+  }
+  &.em {
+    @include cardType(var(--em), var(--em-o));
+  }
+  &__link-container {
     align-items: center;
-    background: #282c35;
-    border-radius: 4px;
-    color: white;
     display: flex;
-    font-family: 'Ubuntu Mono', monospace;
+    &:nth-child(2) {
+      margin-top: 20px;
+    }
+  }
+  &__logo {
+    height: 50px;
+    margin-right: 20px;
+    width: 50px;
+  }
+  &__input {
+    border-radius: 4px;
     font-size: 1.3em;
     height: 50px;
     padding: 0 15px;
     width: 100%;
-    &:focus{
-        outline: none;
+    &.subject {
+      margin-top: 20px;
     }
-    &.fb {
-        border: 4px solid var(--fb);
+  }
+  &__extra-text {
+    & textarea {
+      border-radius: 4px;
+      font-family: "Ubuntu Mono", monospace;
+      font-size: 1.3em;
+      height: 100px;
+      margin: 20px 0;
+      padding: 10px 15px;
+      resize: none;
+      width: 100%;
     }
-    &.tw {
-        border: 4px solid var(--tw);
-    }
-    &.li {
-        border: 4px solid var(--li);
-    }
+  }
+  &__test-btn {
+    border-radius: 4px;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+    font-family: "Ubuntu Mono", monospace;
+    font-size: 1.3em;
+    height: 50px;
+    margin-left: 20px;
+    padding: 0 20px;
+  }
+}
+
+.code-block {
+  align-items: center;
+  background: #282c35;
+  border-radius: 4px;
+  color: white;
+  display: flex;
+  font-family: "Ubuntu Mono", monospace;
+  font-size: 1.3em;
+  height: 50px;
+  padding: 0 15px;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
 }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -5,22 +5,26 @@
       <h1 class="title">Easy Share 🚀</h1>
       <h2 class="subtitle">Generate Social Share Links</h2>
       <p class="intro-text">
-        Made by 
-        <a target="_blank" class="twitter-link" href="https://twitter.com/alexginns">
-          Alex Ginns
-        </a> 
-        ~ with 
-        <a target="_blank" class="npm-link" href="https://www.npmjs.com/package/easy-social-share-links">
-          easy-social-share-links
-        </a>
+        Made by
+        <a
+          target="_blank"
+          class="twitter-link"
+          href="https://twitter.com/alexginns"
+        >Alex Ginns</a>
+        ~ with
+        <a
+          target="_blank"
+          class="npm-link"
+          href="https://www.npmjs.com/package/easy-social-share-links"
+        >easy-social-share-links</a>
       </p>
       <div class="share-cards">
-        <ShareCard 
+        <ShareCard
           :logo="require('~/assets/logos/facebook.svg')"
           :alt="'Facebook Logo'"
           :type="'fb'"
         />
-        <ShareCard 
+        <ShareCard
           :logo="require('~/assets/logos/twitter.svg')"
           :alt="'Twitter Logo'"
           :type="'tw'"
@@ -30,35 +34,47 @@
           :alt="'LinkedIn Logo'"
           :type="'li'"
         />
-        <ShareCard
-          :logo="require('~/assets/logos/email.svg')"
-          :alt="'Email Logo'"
-          :type="'em'"
-        />
+        <ShareCard :logo="require('~/assets/logos/email.svg')" :alt="'Email Logo'" :type="'em'" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import GithubCorner from '~/components/GithubCorner.vue';
-import ShareCard from '~/components/ShareCard.vue';
+import GithubCorner from "~/components/GithubCorner.vue";
+import ShareCard from "~/components/ShareCard.vue";
 
 export default {
   components: {
     GithubCorner,
     ShareCard
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.intro-text{
+.container {
+  width: 100%;
+  padding: 0 20px;
+  margin: 30px auto 30px auto;
+  max-width: 650px;
+}
+
+.title {
+  text-align: center;
+  font-size: 3em;
+}
+
+.subtitle {
+  text-align: center;
+}
+
+.intro-text {
   text-align: center;
   margin-top: 10px;
 }
 .twitter-link {
-  color: #1DA1F2;
+  color: var(--tw);
 }
 .npm-link {
   color: #fb3e44;
