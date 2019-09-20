c55f9f19d80757e16a1a1755c0eeb86a823e2023
Alex Ginns <alexginns@gmail.com>
2019-09-20 21:22:26 +1000

    feat: pwa and favicon (#2, #4)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,55 @@
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: 'Easy Share',
-    meta: [{
-        charset: 'utf-8'
+    title: "Easy Share",
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Easily generate Facebook, Twitter, LinkedIn and Email share links'
+        hid: "description",
+        name: "description",
+        content:
+          "Easily generate Facebook, Twitter, LinkedIn and Email share links"
       }
     ],
-    link: [{
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css?family=Merriweather|Montserrat:900|Ubuntu+Mono&display=swap'
-    }]
+    link: [
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Merriweather|Montserrat:900|Ubuntu+Mono&display=swap"
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
    */
   loading: {
-    color: '#fff'
+    color: "#fff"
   },
   /*
    ** Global CSS
    */
-  css: [
-    '~/assets/styles/global.scss',
-    '~/assets/styles/_vars.scss'
-  ],
+  css: ["~/assets/styles/global.scss", "~/assets/styles/_vars.scss"],
+  /*
+   ** Modules
+   */
+  modules: ["@nuxtjs/pwa"],
   /*
    ** Build configuration
    */
   build: {
-    transpile: ['easy-social-share-links'],
+    transpile: ["easy-social-share-links"],
     /*
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
   }
-}
+};
